5356e87484f53dfe9ed6d09f3d88ec59798956bc
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-08-18 12:18:39 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+
+//crie as variáveis
+var rato,fundo, gato;
+var rato1, rato2, gato1, gato2;
+
+
+function preload() {
+    //carregue as imagens aqui
+    fundo = loadImage("jardim.png");
+
+    //carregue as animações
+    //rato
+  
+
+    //animações do gato
+
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //crie a sprite de gato
+  
+    //adicione as animações
+    
+    //defina a escala
+
+    //defina a velocidade
+
+
+    //crie a sprite do rato
+
+    //adicione as animações
+
+    //defina a escala
+
+    //defina a velocidade
+
+
+}
+
+function draw() {
+
+    background(fundo);
+
+    //Programe para o computador verificar se o gato e o rato colidiram, se sim, instrua o pc para zerar a velocidade do gato e do rato e alterar suas animações.
+
+
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //Para mover e alterar a animação, escreva o código aqui
+
+}
